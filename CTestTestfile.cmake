34cb0b0ae66026bbc1d3642824d9536d5662e357
Assif <Assif@Dr-Hassin.local>
2023-01-24 21:35:00 +0200

    added basic server

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Assif/Documents/Server_client_project
+# Build directory: /Users/Assif/Documents/Server_client_project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("GradeServer")
+subdirs("GradeClient")
---
+++ GradeServer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Assif/Documents/Server_client_project/GradeServer
+# Build directory: /Users/Assif/Documents/Server_client_project/GradeServer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
